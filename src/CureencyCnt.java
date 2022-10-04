ecb4f85a24f4ebf750001c2ce1e2a5f037208e26
lucyoz <lucyoz.kwon@gmail.com>
2022-10-04 14:47:53 +0900

    화폐 문제 커밋

--- src/CureencyCnt.java
+++ src/CureencyCnt.java
@@ -15,22 +15,22 @@ public class CureencyCnt {
         //25,000원을 각 화폐 단위로 나누었을 때 몫과 나머지를 출력하는 코드를 작성해주세요
         //각 화폐 단위는 위와 같이 각각 변수로 선언해주세요.
         System.out.println(refund+"원");
-        System.out.printf("50000원 몫: %d개 나미저: %d \n",refund/curr50000,refund%curr50000);
+        System.out.printf("50000원 몫: %d개 나머지: %d \n", refund/curr50000,refund%curr50000);
         refund %= curr50000;
-        System.out.printf("10000원 몫: %d개 나미저: %d \n",refund/curr10000,refund%curr10000);
+        System.out.printf("10000원 몫: %d개 나머지: %d \n", refund/curr10000,refund%curr10000);
         refund %= curr10000;
-        System.out.printf("5000원 몫: %d개 나미저: %d \n",refund/curr5000,refund%curr5000);
+        System.out.printf("5000원 몫: %d개 나머지: %d \n", refund/curr5000,refund%curr5000);
         refund %= curr5000;
-        System.out.printf("1000원 몫: %d개 나미저: %d \n",refund/curr1000,refund%curr1000);
+        System.out.printf("1000원 몫: %d개 나머지: %d \n", refund/curr1000,refund%curr1000);
         refund %= curr1000;
-        System.out.printf("500원 몫: %d개 나미저: %d \n",refund/curr500,refund%curr500);
+        System.out.printf("500원 몫: %d개 나머지: %d \n", refund/curr500,refund%curr500);
         refund %= curr500;
-        System.out.printf("100원 몫: %d개 나미저: %d \n",refund/curr100,refund%curr100);
+        System.out.printf("100원 몫: %d개 나머지: %d \n", refund/curr100,refund%curr100);
         refund %= curr100;
 
-        System.out.printf("50원 몫: %d개 나미저: %d \n",refund/curr50,refund%curr50);
+        System.out.printf("50원 몫: %d개 나머지: %d \n", refund/curr50,refund%curr50);
         refund %= curr50;
-        System.out.printf("10원 몫: %d개 나미저: %d \n",refund/curr10,refund%curr10);
+        System.out.printf("10원 몫: %d개 나머지: %d \n", refund/curr10,refund%curr10);
         refund %= curr10;
     }
 }
