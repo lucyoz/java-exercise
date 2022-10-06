e131e54555af34fe96f0cf01b136700a90ce3206
lucyoz <lucyoz.kwon@gmail.com>
2022-10-06 15:34:35 +0900

    파일 글자 읽기 1. 두 글자 2. N 글자

--- src/edu1006/file/FileExercise.java
+++ src/edu1006/file/FileExercise.java
@@ -17,9 +17,24 @@ public class FileExercise {
     public char readAChar(String filename) throws IOException {
         FileReader fileReader = new FileReader(filename);
         return (char)fileReader.read();
-
     }
 
+    public String read2Chars(String filename) throws IOException{
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        str += (char) fileReader.read();
+        str += (char) fileReader.read();
+        return str;
+    }
+
+    public String readNChars(String filename, int N) throws IOException{
+        FileReader fileReader = new FileReader(filename);
+        String str = "";
+        for(int i=0;i<N;i++){
+            str += (char) fileReader.read();
+        }
+        return str;
+    }
 
     public static void main(String[] args) throws IOException {
         // ./는 현재 디렉토리
@@ -28,8 +43,15 @@ public class FileExercise {
         char c = fileExercise.readAChar("./a_file.txt");
         System.out.println(c);
 
+        String str = fileExercise.read2Chars("./a_file.txt");
+        System.out.println(str);
+
+        str = fileExercise.readNChars("./a_file.txt", 5);
+        System.out.println(str);
         /*
         h
+        he
+        hello
          */
     }
 }
