ddaee49eed7d4eae8a69989a02591b4236b25a26
lucyoz <lucyoz.kwon@gmail.com>
2022-10-06 13:41:18 +0900

    Map연습 AlphabetMap

--- src/edu1006/Alphabet/AlphabetCnt.java
+++ src/edu1006/Alphabet/AlphabetCnt.java
@@ -1,7 +1,12 @@
 package edu1006.Alphabet;
 
+import java.util.HashMap;
+import java.util.Locale;
+
 public class AlphabetCnt {
-    public static boolean isAlphabet(char c){
+
+    //알파벳인지 아닌지 boolean으로 return ( 아스키코드 이용 -> 그저 char로 비교할 수도 있음)
+    public boolean isAlphabet(char c){
         if ((c >= 65 && c<=90) || (c >=97 && c<=122) ){
             return true;
         }
@@ -9,14 +14,28 @@ public class AlphabetCnt {
     }
 
     public static void main(String[] args) {
-        String s1 = "aabbccddeeffjjkkkll";
+        String s1 = "aabbcd```cdd??ee///ffjjkkkll";
+        s1 = s1.toUpperCase();
+        AlphabetCnt alphabetCnt = new AlphabetCnt();
+
+        HashMap<Character, Integer> alphabetMap = new HashMap<>();
+        for(char c = 'A';c<'Z';c++){
+            alphabetMap.put(c, 0);
+        }
 
         // String을 한글자씩 출력할 수 있어야 한다.
         for(int i=0;i<s1.length();i++){
-            System.out.println(s1.charAt(i));
+            char c1 = s1.charAt(i);
+            boolean isAlphabet = alphabetCnt.isAlphabet(c1);
+            if(isAlphabet){
+                //여기에서 개수를 센다
+
+                alphabetMap.put(c1, alphabetMap.get(c1)+1);
+
+            }
         }
 
-        System.out.println(isAlphabet('Z'));
+        System.out.println(alphabetMap);
 
     }
 }
