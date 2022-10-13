a1f8e70ad97e609ce6590477a28ccdb031d2f8d8
lucyoz <lucyoz.kwon@gmail.com>
2022-10-13 09:46:22 +0900

    selectionSort 01

--- src/edu1013/SelectionSort01.java
+++ src/edu1013/SelectionSort01.java
@@ -5,18 +5,15 @@ import java.util.Arrays;
 public class SelectionSort01 {
     //가장 작은 숫자를 0번째에
     public int[] sort1(int[] arr){
-        int minI = 0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[minI]>arr[i]){
-                minI = i;
+        int i=0;
+            for(int j=1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
             }
-        }
 
-        if (arr[minI] != arr[0]){
-            int tmp = arr[0];
-            arr[0] = arr[minI];
-            arr[minI] = tmp;
-        }
         return arr;
     }
 
