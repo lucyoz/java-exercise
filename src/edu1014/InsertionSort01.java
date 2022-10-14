2579be3fcccbd1157a624988e44c23c8f965be9b
lucyoz <lucyoz.kwon@gmail.com>
2022-10-14 10:26:25 +0900

    insertionSort

--- src/edu1014/InsertionSort01.java
+++ src/edu1014/InsertionSort01.java
@@ -3,13 +3,15 @@ package edu1014;
 import java.util.Arrays;
 
 public class InsertionSort01 {
-    public int[] sort(int[] arr, int i){
-
-        int j = i;
-        if(arr[j] < arr[j-1]){
-            int tmp = arr[j];
-            arr[j] = arr[j-1];
-            arr[j-1] = tmp;
+    public int[] sort(int[] arr){
+        for(int i=1;i<arr.length;i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = tmp;
+                }
+            }
         }
 
         return arr;
@@ -19,7 +21,7 @@ public class InsertionSort01 {
         int[] arr = {8, 5, 6, 2, 4};
 
         InsertionSort01 insertionSort = new InsertionSort01();
-        int[] result = insertionSort.sort(arr, 1);
+        int[] result = insertionSort.sort(arr);
 
         System.out.println(Arrays.toString(result));
     }
