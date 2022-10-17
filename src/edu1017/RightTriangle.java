9e01001e3434ab6f37dcfefeaf0306078b3ad5a7
lucyoz <lucyoz.kwon@gmail.com>
2022-10-17 09:21:32 +0900

    RightTriangle

--- src/edu1017/RightTriangle.java
+++ src/edu1017/RightTriangle.java
@@ -1,15 +1,19 @@
 package edu1017;
 
 public class RightTriangle {
-    public static void main(String[] args) {
-        String star = "*";
-
-        for(int i=1;i<5;i++){
-            for(int j = i; j>0;j--) {
-                System.out.print(star);
+    public void draw(int n){
+        for(int i = 0;i<n;i++){
+            for(int j=i;j>=0;j--){
+                System.out.print("*");
             }
             System.out.println();
         }
+    }
+
+    public static void main(String[] args) {
+        RightTriangle rightTriangle = new RightTriangle();
+
+        rightTriangle.draw(5);
     }
 
 }
